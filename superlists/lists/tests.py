52d8ca56a054dddff8ec5c935efb7b6b933a58e6
Marcel Heinz <marcel.heinz@gmail.com>
2020-05-04 16:28:06 +0200

    Updated templates, tests, views and urls

--- superlists/lists/tests.py
+++ superlists/lists/tests.py
@@ -45,9 +45,8 @@ class NewListTest(TestCase):
         response = self.client.post('/lists/new',
                                     data={'item_text': 'A new list '
                                                        'item'})
-        self.assertEqual(response.status_code, 302)
-        self.assertEqual(response['location'],
-                         '/lists/the-only-list-in-the-world/')
+        self.assertRedirects(response,
+                             '/lists/the-only-list-in-the-world/')
 
 
 class ItemModelTest(TestCase):
