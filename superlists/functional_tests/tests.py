95c945603d2ea4f68e6063b304c7a6d10d449785
Marcel Heinz <marcel.heinz@gmail.com>
2020-05-04 14:17:28 +0200

    Updated functional test and removed sleeps

--- superlists/functional_tests/tests.py
+++ superlists/functional_tests/tests.py
@@ -8,8 +8,11 @@ NewVisitorTest tests the behaviour of the website.
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 import time
-import unittest
 from django.test import LiveServerTestCase
+
+from selenium.common.exceptions import WebDriverException
+
+MAX_WAIT = 10
 
 
 class NewVisitorTest(LiveServerTestCase):
@@ -19,10 +22,19 @@ class NewVisitorTest(LiveServerTestCase):
     def tearDown(self):
         self.browser.quit()
 
-    def check_for_row_in_list_table(self, row_text):
-        table = self.browser.find_element_by_id('id_list_table')
-        rows = table.find_elements_by_tag_name('tr')
-        self.assertIn(row_text, [row.text for row in rows])
+    def wait_for_row_in_list_table(self, row_text):
+        start_time = time.time()
+        while True:
+            try:
+                table = self.browser.find_element_by_id(
+                    'id_list_table')
+                rows = table.find_elements_by_tag_name('tr')
+                self.assertIn(row_text, [row.text for row in rows])
+                return
+            except (AssertionError, WebDriverException) as e:
+                if time.time() - start_time > MAX_WAIT:
+                    raise e
+                time.sleep(0.5)
 
     def test_can_start_a_list_and_retrieve_it_later(self):
         # Marcel has heard a cool new online to-do app.
@@ -45,20 +57,18 @@ class NewVisitorTest(LiveServerTestCase):
         # When he hits enter, the page updates, and now the page lists
         # 1. 'Buy peacock feathers' as an item in the to-do list
         inputbox.send_keys(Keys.ENTER)
-        time.sleep(1)
 
-        self.check_for_row_in_list_table('1. Buy peacock feathers')
+        self.wait_for_row_in_list_table('1. Buy peacock feathers')
 
         # There is still a text box inviting him to add another item.
         # He enters 'Use peacock feathers to make a fly'
         inputbox = self.browser.find_element_by_id('id_new_item')
         inputbox.send_keys('Use peacock feathers to make a fly')
         inputbox.send_keys(Keys.ENTER)
-        time.sleep(1)
 
         # The page updates again, and now shows both items in her list
-        self.check_for_row_in_list_table('1. Buy peacock feathers')
-        self.check_for_row_in_list_table(
+        self.wait_for_row_in_list_table('1. Buy peacock feathers')
+        self.wait_for_row_in_list_table(
             '2. Use peacock feathers to make a fly')
 
         # Marcel wonders whether the site will remember his list.
