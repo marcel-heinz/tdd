fc708e62792220888adf5c64117d2a7d7537b8ae
Marcel Heinz <marcel.heinz@gmail.com>
2020-05-04 14:52:56 +0200

    Added further tests to the functional part

--- superlists/functional_tests/tests.py
+++ superlists/functional_tests/tests.py
@@ -36,7 +36,7 @@ class NewVisitorTest(LiveServerTestCase):
                     raise e
                 time.sleep(0.5)
 
-    def test_can_start_a_list_and_retrieve_it_later(self):
+    def test_can_start_a_list_for_one_user(self):
         # Marcel has heard a cool new online to-do app.
         # He goes to check out its homepage
         self.browser.get(self.live_server_url)
@@ -76,7 +76,49 @@ class NewVisitorTest(LiveServerTestCase):
         # for his list -- there is some
         # explanatory text to that effect.
 
-        self.fail('Finish the test')
+        # self.fail('Finish the test')
         # He visits that URL - his to-do list is still there
 
         # Satisfied. Quits the browser.
+
+    def test_multiple_users_can_start_lists_at_different_urls(self):
+        # Marcel starts a new to-do list
+        self.browser.get(self.live_server_url)
+        inputbox = self.browser.find_element_by_id('id_new_item')
+        inputbox.send_keys('Buy peacock feathers')
+        inputbox.send_keys(Keys.ENTER)
+        self.wait_for_row_in_list_table('1. Buy peacock feathers')
+
+        # He notices that his list has a unique URL
+        marcel_list_url = self.browser.current_url
+        self.assertRegex(marcel_list_url, '/lists/.+')
+
+        # Now a new user, Maria, comes along to the site.
+        ## We use a new browser session to make sure that no
+        ## information of Marcel is coming through from cookies etc.
+
+        self.browser.quit()
+
+        self.browser.get(self.live_server_url)
+        page_text = self.browser.find_element_by_tag_name('body').text
+        self.assertNotIn('Buy peacock feathers', page_text)
+        self.assertNotIn('make a fly', page_text)
+
+        # Maria starts a new list by entering a new item
+        # The list is less interesting than Marcel's...
+        inputbox = self.browser.find_element_by_id('id_new_item')
+        inputbox.send_keys('Buy milk')
+        inputbox.send_keys(Keys.ENTER)
+        self.wait_for_row_in_list_table('1. Buy milk')
+
+        # Maria gets her own unique URL
+        maria_list_url = self.browser.current_url
+        self.assertRegex(marcel_list_url, '/lists/.+')
+        self.assertNotEqual(marcel_list_url, maria_list_url)
+
+        # Again, there is no trace of Marcel's list
+        page_text = self.browser.find_element_by_tag_name('body').text
+        self.assertNotIn('Buy peacock feathers', page_text)
+        self.assertIn('Buy milk', page_text)
+
+        # Satisfied, they both go back to sleep
